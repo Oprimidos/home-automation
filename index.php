887b5331490f81fa12da3b55393c478626f35d8e
Oprimidos <83984387+Oprimidos@users.noreply.github.com>
2023-04-17 16:35:44 +0300

    Update index.php
    producer login ve consumer login ksımları buton haline getirildi ve butonları çalıştıracak js kodu eklendi

--- index.php
+++ index.php
@@ -3,22 +3,31 @@
 <head>
     <title>Home Automation</title>
     <link rel="stylesheet" href="css/styles.css">
+   
 </head>
 <body>
     <header>
         <h1>Home Automation</h1>
     </header>
     <div class="container">
-        <div id="index-info">
-        <p>Welcome to our Home Automation software! </p><p>This system allows you to control various devices in your home, such as lights, temperature, and humidity, from a single dashboard.</p><p>To get started, please log in as either a Producer or Consumer:</p></div>
-        <div id="index-login">
-        <button>
-           <a href="login.php?role=producer"> Producer Login</a>
-        </button>
-        <button>
-            <a href="login.php?role=consumer">Consumer Login</a>
-        </button></div>
-        
+        <p>Welcome to our Home Automation software! This system allows you to control various devices in your home, such as lights, temperature, and humidity, from a single dashboard.</p>
+        <div class="buttons-container">
+            <button id="producer-login">Producer Login</button>
+            <button id="consumer-login">Consumer Login</button>
+        </div>
     </div>
+
+    
+    <script>
+        var producerLoginBtn = document.getElementById("producer-login");
+        producerLoginBtn.onclick = function() {
+            window.location.href = "login.php?role=producer";
+        };
+
+        var consumerLoginBtn = document.getElementById("consumer-login");
+        consumerLoginBtn.onclick = function() {
+            window.location.href = "login.php?role=consumer";
+        };
+    </script>
 </body>
 </html>
