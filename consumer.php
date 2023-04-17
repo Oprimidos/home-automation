eb0dcafea90a5503e8dda60ef64d7c4a73e53787
Oprimidos <83984387+Oprimidos@users.noreply.github.com>
2023-04-17 12:01:49 +0300

    Update consumer.php
    datayı okurken hata veriyordu, düzelttim

--- consumer.php
+++ consumer.php
@@ -1,41 +1,40 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Home Automation - Consumer Dashboard</title>
+    <title>Consumer Dashboard</title>
     <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
-    <header>
-        <h1>Home Automation - Consumer Dashboard</h1>
-        <nav>
-            <ul>
-                <li><a href="index.php">Home</a></li>
-                <li><a href="logout.php">Logout</a></li>
-            </ul>
-        </nav>
-    </header>
     <div class="container">
-        <h2>Latest Sensor Readings</h2>
+        <h1>Consumer Dashboard</h1>
         <table>
             <thead>
                 <tr>
-                    <th>Type</th>
-                    <th>Value</th>
+                    <th>Measurement</th>
+                    <th>Reading</th>
                     <th>Timestamp</th>
                 </tr>
             </thead>
             <tbody>
                 <?php
-                    $sensor_data = json_decode(file_get_contents('mock_data/sensor_readings.json'), true);
-                    $latest_readings = array();
-                    foreach ($sensor_data as $reading) {
-                        if (!array_key_exists($reading['type'], $latest_readings) || $reading['timestamp'] > $latest_readings[$reading['type']]['timestamp']) {
-                            $latest_readings[$reading['type']] = $reading;
-                        }
+                $json = file_get_contents("mock_data/sensor_readings.json");
+                $data = json_decode($json, true);
+
+                foreach($data as $key => $value) {
+                    echo "<tr>";
+                    echo "<td>" . ucfirst($key) . "</td>";
+                    if(isset($value['reading'])) {
+                        echo "<td>" . $value['reading'] . "</td>";
+                    } else {
+                        echo "<td>-</td>";
                     }
-                    foreach ($latest_readings as $reading) {
-                        echo "<tr><td>" . ucfirst($reading['type']) . "</td><td>" . $reading['value'] . "</td><td>" . date('Y-m-d H:i:s', $reading['timestamp']) . "</td></tr>";
+                    if(isset($value['timestamp'])) {
+                        echo "<td>" . $value['timestamp'] . "</td>";
+                    } else {
+                        echo "<td>-</td>";
                     }
+                    echo "</tr>";
+                }
                 ?>
             </tbody>
         </table>
