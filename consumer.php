6e9f9835dbd5b8e62a0d6b657a3da7d8c78a2754
Oprimidos <83984387+Oprimidos@users.noreply.github.com>
2023-04-17 18:27:26 +0300

    Update consumer.php
    logout sayfası yerine index.php ye yönelndirecek

--- consumer.php
+++ consumer.php
@@ -8,7 +8,7 @@
     <a href="#" class="navbar-logo">Home Automation</a>
     <ul class="navbar-menu">
       <li class="navbar-item"><a href="index.php">Home</a></li>
-      <li class="navbar-item"><a href="logout.php">Logout</a></li>
+      <li class="navbar-item"><a href="index.php">Logout</a></li>
     </ul>
   </nav>
 </header>
