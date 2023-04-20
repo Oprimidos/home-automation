30c865ad9a9d9b0758d78bb649849013b1b8afd4
Oprimidos <83984387+Oprimidos@users.noreply.github.com>
2023-04-20 23:14:16 +0300

    Update consumer.php
    butona tıkladığımda onun verilerini gösteren kod yazdım bir de dataları tek tablo yerine ayrı ayrı tablolarda yazdırdım

--- consumer.php
+++ consumer.php
@@ -4,8 +4,8 @@
     <title>Consumer Dashboard</title>
     <link rel="stylesheet" href="css/styles.css">
     <header>
-  <?php include("navbar.html"); ?>
-</header>
+        <?php include("navbar.html"); ?>
+    </header>
 </head>
 <body>
     <div class="container">
@@ -14,8 +14,15 @@
         <br>
         <br>
         <br>
-        <h1 style="padding-top:30px; padding-bottom:30px;" >Consumer Dashboard</h1>
-        <table>
+        <h1 style="padding-top:30px; padding-bottom:30px;">Consumer Dashboard</h1>
+
+        
+        <button onclick="showTable('temperature')">Temperature</button>
+        <button onclick="showTable('humidity')">Humidity</button>
+        <button onclick="showTable('light')">Light</button>
+
+        <!-- Temperature Table -->
+        <table id="temperature-table">
             <thead>
                 <tr>
                     <th>Measurement</th>
@@ -24,31 +31,18 @@
                 </tr>
             </thead>
             <tbody>
-                <?php
+                <?php                
                 $json = file_get_contents("mock_data/sensor_readings.json");
                 $data = json_decode($json, true);
 
                 foreach($data as $key => $value) {
+                    if ($key == "temperature") {
                         echo "<tr>";
                         echo "<td>";
-                        if ($key == "temperature") {
-                            echo "Temperature (C)";
-                        } else if ($key == "humidity") {
-                            echo "Humidity (%)";
-                        } else {
-                            echo ucfirst($key);
-                        }
+                        echo "Temperature (C)";
                         echo "</td>";
                         if (isset($value['reading'])) {
-                            if ($key == "temperature") {
-                                echo "<td>" . round(($value['reading'] )) . " C</td>";
-                            } else if ($key == "humidity") {
-                                echo "<td>" . $value['reading'] . " %</td>";
-                            } else if ($key == "light") {
-                                echo "<td>" . ($value['reading'] ? "On" : "Off") . "</td>";
-                            } else {
-                                echo "<td>" . $value['reading'] . "</td>";
-                            }
+                            echo "<td>" . round(($value['reading'] )) . " C</td>";
                         } else {
                             echo "<td>-</td>";
                         }
@@ -59,9 +53,91 @@
                         }
                         echo "</tr>";
                     }
+                }
                 ?>
             </tbody>
         </table>
+
+        <!-- Humidity Table -->
+        <table id="humidity-table" style="display:none;">
+            <thead>
+                <tr>
+                    <th>Measurement</th>
+                    <th>Reading</th>
+                    <th>Timestamp</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php                
+                foreach($data as $key => $value) {
+                    if ($key == "humidity") {
+                        echo "<tr>";
+                        echo "<td>";
+                        echo "Humidity (%)";
+                        echo "</td>";
+                        if (isset($value['reading'])) {
+                            echo "<td>" . $value['reading'] . " %</td>";
+                        } else {
+                            echo "<td>-</td>";
+                        }
+                        if (isset($value['timestamp'])) {
+                            echo "<td>" . $value['timestamp'] . "</td>";
+                        } else {
+                            echo "<td>-</td>";
+                        }
+                        echo "</tr>";
+                    }
+                }
+                ?>
+            </tbody>
+        </table>
+
+        <!-- Light Table -->
+        <table id="light-table" style="display:none;">
+            <thead>
+                <tr>
+                    <th>Measurement</th>
+                    <th>Reading</th>
+                    <th>Timestamp</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php                
+                foreach($data as $key => $value) {
+                    if ($key == "light") {
+                        echo "<tr>";
+                        echo "<td>";
+                        echo "Light";
+                        echo "</td>";
+                        if (isset($value['reading'])) {
+                            echo "<td>" . ($value['reading'] ? "On" : "Off") . "</td>";
+                        } else {
+                            echo "<td>-</td>";
+                        }
+                        if (isset($value['timestamp'])) {
+                            echo "<td>" . $value['timestamp'] . "</td>";
+                        } else {
+                            echo "<td>-</td>";
+                        }
+                        echo "</tr>";
+                    }
+                }
+                ?>
+            </tbody>
+        </table>
+
     </div>
+
+    <script>
+        function showTable(tableId) {
+            var table = document.getElementById(tableId + "-table");
+            if (table.style.display === "none") {
+                table.style.display = "block";
+            } else {
+                table.style.display = "none";
+            }
+        }
+    </script>
+
 </body>
 </html>
