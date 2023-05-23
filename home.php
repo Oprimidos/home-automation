3f7007b519d52c7ba8432c5760c047158ee738a3
Oprimidos <83984387+Oprimidos@users.noreply.github.com>
2023-05-23 22:18:28 +0300

    consumer ve producer için home sayfaları eklendi

---
+++ home.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Consumer Dashboard</title>
+    <link rel="stylesheet" href="assets/css/home.css">
+    <header>
+        <?php include("navbar.php"); ?>
+    </header>
+   <style>
+    body{background-image: url('./assets/images/house.jpg');}
+    </style>
+</head>
+
+<body>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <div class="container">
+        <div class="box">
+            <a href="living.php">
+            <img src="./assets/images/living.jpg" alt="Living Room">
+            <h1>Living</h1>
+    </a>
+        </div>
+        <div class="box">
+        <a href="kitchen.php">
+            <img src="./assets/images/kitchen.jpg" alt="Kitchen">
+            <h1>Kitchen</h1>
+            </a>
+        </div>
+        <div class="box">
+        <a href="children.php">
+            <img src="./assets/images/children.jpg" alt="Children Room">
+            <h1>Children Room</h1>
+            </a>
+        </div>
+        <div class="box">
+        <a href="bedroom.php">
+            <img src="./assets/images/bedroom.jpg" alt="Bedroom">
+            <h1>Bedroom</h1>
+</a>
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ homeP.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Consumer Dashboard</title>
+    <link rel="stylesheet" href="assets/css/home.css">
+    <header>
+        <?php include("navbar.php"); ?>
+    </header>
+   <style>
+    body{background-image: url('./assets/images/house.jpg');}
+    </style>
+</head>
+
+<body>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <div class="container">
+        <div class="box">
+            <a href="livingP.php">
+            <img src="./assets/images/living.jpg" alt="Living Room">
+            <h1>Living</h1>
+    </a>
+        </div>
+        <div class="box">
+        <a href="kitchenP.php">
+            <img src="./assets/images/kitchen.jpg" alt="Kitchen">
+            <h1>Kitchen</h1>
+            </a>
+        </div>
+        <div class="box">
+        <a href="childrenP.php">
+            <img src="./assets/images/children.jpg" alt="Children Room">
+            <h1>Children Room</h1>
+            </a>
+        </div>
+        <div class="box">
+        <a href="bedroomP.php">
+            <img src="./assets/images/bedroom.jpg" alt="Bedroom">
+            <h1>Bedroom</h1>
+</a>
+        </div>
+    </div>
+</body>
+
+</html>
