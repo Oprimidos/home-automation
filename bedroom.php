b89a795524250c150db45f4ff356ee93c7974a87
Mustafa Esen <mesen1100@gmail.com>
2023-05-24 23:51:28 +0300

    Add new room features

--- bedroom.php
+++ bedroom.php
@@ -11,103 +11,67 @@
 
 <body>
     <?php
-    $sqlheat = "SELECT * FROM heatview";
-    $queryheat = $db->prepare($sqlheat);
-    $queryheat->execute();
-    $heat = $queryheat->fetchAll(PDO::FETCH_ASSOC);
+    $sqlbedroom = "SELECT * FROM roomview WHERE roomID=4";
+    $querybedroom = $db->prepare($sqlbedroom);
+    $querybedroom->execute();
+    $bedroom = $querybedroom->fetchAll(PDO::FETCH_ASSOC);
 
-    $sqllight = "SELECT * FROM lightview";
-    $querylight = $db->prepare($sqllight);
-    $querylight->execute();
-    $light = $querylight->fetchAll(PDO::FETCH_ASSOC);
-
-    $sqlair = "SELECT * FROM airview";
-    $queryair = $db->prepare($sqlair);
-    $queryair->execute();
-    $air = $queryair->fetchAll(PDO::FETCH_ASSOC);
-
-    $sqlroom = "SELECT * FROM room";
-    $queryroom = $db->prepare($sqlroom);
-    $queryroom->execute();
-    $room = $queryroom->fetchAll(PDO::FETCH_ASSOC);
+    $query=$db->prepare("UPDATE aircondition SET airTime=:airTime WHERE airID=:airID");
+    $currentTime = date("Y-m-d H:i:s");
+    $eklendi=$query->execute(array(
+        "airTime"=>$currentTime,
+        "airID"=>4      
+    ));
     ?>
-    <div class="container">
+    <div class="container" ">
         <br>
         <br>
         <br>
         <br>
         <br>
-        <h1 style="padding-top:30px; padding-bottom:30px;">Consumer Dashboard</h1>
+        <h1 style=" padding-top:30px; padding-bottom:30px;">Consumer Dashboard</h1>
+        
 
-
-        <button onclick="showTable('temperature')">Temperature</button>
-        <button onclick="showTable('humidity')">Humidity</button>
-        <button onclick="showTable('light')">Light</button>
-        <!-- Temperature Table -->
-        <table id="temperature-table">
-            <thead>
-                <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">Reading</th>
-                    <th scope="col">Time</th>
-                    <th scope="col">Room</th>
-                </tr>
-            </thead>
-            <tbody class="table-group-divider">
-                <?php foreach ($heat as $row) { ?>
-                    <tr>
-                        <th scope="row"><?php echo $row['heatID'] ?></th>
-                        <td><?php echo $row['heatValue'] ?></td>
-                        <td>Time yazılacak</td>
-                        <td><?php echo $row['roomName'] ?></td>
-                    </tr>
-                <?php } ?>
-            </tbody>
-        </table>
-
-        <!-- Humidity Table -->
-        <table id="humidity-table" style="display:none;">
-            <thead>
-                <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">Reading</th>
-                    <th scope="col">Time</th>
-                    <th scope="col">Room</th>
-                </tr>
-            </thead>
-            <tbody class="table-group-divider">
-                <?php foreach ($air as $row) { ?>
-                    <tr>
-                        <th scope="row"><?php echo $row['airID'] ?></th>
-                        <td><?php echo $row['airValue'] ?></td>
-                        <td>Time yazılacak</td>
-                        <td><?php echo $row['roomName'] ?></td>
-                    </tr>
-                <?php } ?>
-            </tbody>
-        </table>
-
-        <!-- Light Table -->
-        <table id="light-table" style="display:none;">
-            <thead>
-                <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">Reading</th>
-                    <th scope="col">Time</th>
-                    <th scope="col">Room</th>
-                </tr>
-            </thead>
-            <tbody class="table-group-divider">
-                <?php foreach ($light as $row) { ?>
-                    <tr>
-                        <th scope="row"><?php echo $row['lightID'] ?></th>
-                        <td><?php echo $row['lightValue'] ?></td>
-                        <td>Time yazılacak</td>
-                        <td><?php echo $row['roomName'] ?></td>
-                    </tr>
-                <?php } ?>
-            </tbody>
-        </table>
+        <div class="card text-bg-dark">
+            <img src="assets/images/bedroom.jpg" class="card-img" alt="...">
+            <div class="card-img-overlay">
+                <h5 class="card-title bg-dark">BEDROOM</h5>
+                <?php  foreach($bedroom as $row){?>
+                <table class="table table-dark table-striped">
+                    <thead>
+                        <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">SENSOR</th>
+                            <th scope="col">VALUE</th>
+                            <th scope="col">LAST CHANGE TIME</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row"><?php echo $row['airID']?></th>
+                            <td>AIR CONDITION</td>
+                            <td><?php echo $row['airValue']?></td>
+                            <td><?php echo $row['airTime']?></td>
+                            
+                        </tr>
+                        <tr>
+                            <th scope="row"><?php echo $row['heatID']?></th>
+                            <td>HEAT</td>
+                            <td><?php echo $row['heatValue']?></td>
+                            <td><?php echo $row['heatTime']?></td>
+                            
+                        </tr>
+                        <tr>
+                            <th scope="row"><?php echo $row['lightID']?></th>
+                            <td>LIGHT</td>
+                            <td><?php echo $row['lightValue']?></td>
+                            <td><?php echo $row['lightTime']?></td>
+                        </tr>
+                    </tbody>
+                </table>
+                <?php }?>
+            </div>
+        </div>
 
     </div>
 
