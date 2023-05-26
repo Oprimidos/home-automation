c7c07c8c5e45db04896776e008fe6c43abd99bf2
Mustafa Esen <mesen1100@gmail.com>
2023-05-26 17:39:54 +0300

    Fix some details

--- bedroom.php
+++ bedroom.php
@@ -8,11 +8,10 @@
         <?php include("navbar.php"); ?>
     </header>
     <style>
-    body{background-image: url('./assets/images/bedroom.jpg');}
-   
-
-
-        </style>
+        body {
+            background-image: url('./assets/images/bedroom.jpg');
+        }
+    </style>
 </head>
 
 <body>
@@ -22,11 +21,11 @@
     $querybedroom->execute();
     $bedroom = $querybedroom->fetchAll(PDO::FETCH_ASSOC);
 
-    $query=$db->prepare("UPDATE aircondition SET airTime=:airTime WHERE airID=:airID");
+    $query = $db->prepare("UPDATE aircondition SET airTime=:airTime WHERE airID=:airID");
     $currentTime = date("Y-m-d H:i:s");
-    $eklendi=$query->execute(array(
-        "airTime"=>$currentTime,
-        "airID"=>4      
+    $eklendi = $query->execute(array(
+        "airTime" => $currentTime,
+        "airID" => 4
     ));
     ?>
     <div class="container" ">
@@ -38,12 +37,12 @@
       
         
 
-        <div class="card text-bg-dark">
-            
-            <div class="card-img-overlay">
+        <div class=" card text-bg-dark">
+
+        <div class="card-img-overlay">
             <h1 class="card-title bg-dark" style=" padding-top:30px; padding-bottom:30px; text-align: center;">Consumer Dashboard</h1>
-                <h3 class="card-title bg-dark" style=" padding-top:30px; padding-bottom:30px; text-align: center; ">BEDROOM</h3>
-                <?php  foreach($bedroom as $row){?>
+            <h3 class="card-title bg-dark" style=" padding-top:30px; padding-bottom:30px; text-align: center; ">BEDROOM</h3>
+            <?php foreach ($bedroom as $row) { ?>
                 <table class="table table-dark table-striped">
                     <thead>
                         <tr>
@@ -55,43 +54,33 @@
                     </thead>
                     <tbody>
                         <tr>
-                            <th scope="row"><?php echo $row['airID']?></th>
+                            <th scope="row"><?php echo $row['airID'] ?></th>
                             <td>AIR CONDITION</td>
-                            <td><?php echo $row['airValue']?></td>
-                            <td><?php echo $row['airTime']?></td>
-                            
+                            <td><?php echo $row['airValue'] ?></td>
+                            <td><?php echo $row['airTime'] ?></td>
+
                         </tr>
                         <tr>
-                            <th scope="row"><?php echo $row['heatID']?></th>
+                            <th scope="row"><?php echo $row['heatID'] ?></th>
                             <td>HEAT</td>
-                            <td><?php echo $row['heatValue']?></td>
-                            <td><?php echo $row['heatTime']?></td>
-                            
+                            <td><?php echo $row['heatValue'] ?></td>
+                            <td><?php echo $row['heatTime'] ?></td>
+
                         </tr>
                         <tr>
-                            <th scope="row"><?php echo $row['lightID']?></th>
+                            <th scope="row"><?php echo $row['lightID'] ?></th>
                             <td>LIGHT</td>
-                            <td><?php echo $row['lightValue']?></td>
-                            <td><?php echo $row['lightTime']?></td>
+                            <td><?php echo $row['lightValue'] ?></td>
+                            <td><?php echo $row['lightTime'] ?></td>
                         </tr>
                     </tbody>
                 </table>
-                <?php }?>
-            </div>
+            <?php } ?>
         </div>
+    </div>
 
     </div>
 
-    <script>
-        function showTable(tableId) {
-            var table = document.getElementById(tableId + "-table");
-            if (table.style.display === "none") {
-                table.style.display = "block";
-            } else {
-                table.style.display = "none";
-            }
-        }
-    </script>
 
 </body>
 
