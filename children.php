1b41fbf4fd5cb1cd332744697f591bdfb1da87a4
Mustafa Esen <mesen1100@gmail.com>
2023-05-26 17:43:05 +0300

    Change body part

--- children.php
+++ children.php
@@ -7,120 +7,74 @@
     <header>
         <?php include("navbar.php"); ?>
     </header>
+    <style>
+        body {
+            background-image: url('./assets/images/living.jpg');
+        }
+    </style>
 </head>
 
 <body>
     <?php
-    $sqlheat = "SELECT * FROM heatview";
-    $queryheat = $db->prepare($sqlheat);
-    $queryheat->execute();
-    $heat = $queryheat->fetchAll(PDO::FETCH_ASSOC);
+    $sqlchildren = "SELECT * FROM roomview WHERE roomID=3";
+    $querychildren = $db->prepare($sqlchildren);
+    $querychildren->execute();
+    $children = $querychildren->fetchAll(PDO::FETCH_ASSOC);
 
-    $sqllight = "SELECT * FROM lightview";
-    $querylight = $db->prepare($sqllight);
-    $querylight->execute();
-    $light = $querylight->fetchAll(PDO::FETCH_ASSOC);
-
-    $sqlair = "SELECT * FROM airview";
-    $queryair = $db->prepare($sqlair);
-    $queryair->execute();
-    $air = $queryair->fetchAll(PDO::FETCH_ASSOC);
-
-    $sqlroom = "SELECT * FROM room";
-    $queryroom = $db->prepare($sqlroom);
-    $queryroom->execute();
-    $room = $queryroom->fetchAll(PDO::FETCH_ASSOC);
     ?>
-    <div class="container">
+    <div class="container" ">
         <br>
         <br>
         <br>
         <br>
         <br>
-        <h1 style="padding-top:30px; padding-bottom:30px;">Consumer Dashboard</h1>
+      
+        
 
+        <div class=" card text-bg-dark">
 
-        <button onclick="showTable('temperature')">Temperature</button>
-        <button onclick="showTable('humidity')">Humidity</button>
-        <button onclick="showTable('light')">Light</button>
-        <!-- Temperature Table -->
-        <table id="temperature-table">
-            <thead>
-                <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">Reading</th>
-                    <th scope="col">Time</th>
-                    <th scope="col">Room</th>
-                </tr>
-            </thead>
-            <tbody class="table-group-divider">
-                <?php foreach ($heat as $row) { ?>
-                    <tr>
-                        <th scope="row"><?php echo $row['heatID'] ?></th>
-                        <td><?php echo $row['heatValue'] ?></td>
-                        <td>Time yazılacak</td>
-                        <td><?php echo $row['roomName'] ?></td>
-                    </tr>
-                <?php } ?>
-            </tbody>
-        </table>
+        <div class="card-img-overlay">
+            <h1 class="card-title bg-dark" style=" padding-top:30px; padding-bottom:30px; text-align: center;">Consumer Dashboard</h1>
+            <h3 class="card-title bg-dark" style=" padding-top:30px; padding-bottom:30px; text-align: center; ">CHILDREN ROOM</h3>
+            <?php foreach ($children as $row) { ?>
+                <table class="table table-dark table-striped">
+                    <thead>
+                        <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">SENSOR</th>
+                            <th scope="col">VALUE</th>
+                            <th scope="col">LAST CHANGE TIME</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row"><?php echo $row['airID'] ?></th>
+                            <td>AIR CONDITION</td>
+                            <td><?php echo $row['airValue'] ?></td>
+                            <td><?php echo $row['airTime'] ?></td>
 
-        <!-- Humidity Table -->
-        <table id="humidity-table" style="display:none;">
-            <thead>
-                <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">Reading</th>
-                    <th scope="col">Time</th>
-                    <th scope="col">Room</th>
-                </tr>
-            </thead>
-            <tbody class="table-group-divider">
-                <?php foreach ($air as $row) { ?>
-                    <tr>
-                        <th scope="row"><?php echo $row['airID'] ?></th>
-                        <td><?php echo $row['airValue'] ?></td>
-                        <td>Time yazılacak</td>
-                        <td><?php echo $row['roomName'] ?></td>
-                    </tr>
-                <?php } ?>
-            </tbody>
-        </table>
+                        </tr>
+                        <tr>
+                            <th scope="row"><?php echo $row['heatID'] ?></th>
+                            <td>HEAT</td>
+                            <td><?php echo $row['heatValue'] ?></td>
+                            <td><?php echo $row['heatTime'] ?></td>
 
-        <!-- Light Table -->
-        <table id="light-table" style="display:none;">
-            <thead>
-                <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">Reading</th>
-                    <th scope="col">Time</th>
-                    <th scope="col">Room</th>
-                </tr>
-            </thead>
-            <tbody class="table-group-divider">
-                <?php foreach ($light as $row) { ?>
-                    <tr>
-                        <th scope="row"><?php echo $row['lightID'] ?></th>
-                        <td><?php echo $row['lightValue'] ?></td>
-                        <td>Time yazılacak</td>
-                        <td><?php echo $row['roomName'] ?></td>
-                    </tr>
-                <?php } ?>
-            </tbody>
-        </table>
+                        </tr>
+                        <tr>
+                            <th scope="row"><?php echo $row['lightID'] ?></th>
+                            <td>LIGHT</td>
+                            <td><?php echo $row['lightValue'] ?></td>
+                            <td><?php echo $row['lightTime'] ?></td>
+                        </tr>
+                    </tbody>
+                </table>
+            <?php } ?>
+        </div>
+    </div>
 
     </div>
 
-    <script>
-        function showTable(tableId) {
-            var table = document.getElementById(tableId + "-table");
-            if (table.style.display === "none") {
-                table.style.display = "block";
-            } else {
-                table.style.display = "none";
-            }
-        }
-    </script>
 
 </body>
 
