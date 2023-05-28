cce8f4272457e6011bc9c4e2bab288e7f18666ec
Mustafa Esen <mesen1100@gmail.com>
2023-05-28 15:40:39 +0300

    Fix View

--- children.php
+++ children.php
@@ -20,30 +20,30 @@
 </head>
 
 <body>
-<?php
+    <?php
     $sqlchildren = "SELECT * FROM viewconsumer WHERE roomID=3";
     $querychildren = $db->prepare($sqlchildren);
     $querychildren->execute();
     $children = $querychildren->fetch(PDO::FETCH_ASSOC);
 
     if ($children['airValue'] > 16) {
-        $kwh=$children['airKwh']+0.2;
-        $money=$kwh*0.2;
+        $kwh = $children['airKwh'] + 0.2;
+        $money = $kwh * 0.2;
         $energyair = $db->prepare("UPDATE aircondition SET airKwh=:airKwh,airMoney=:airMoney WHERE airID=:airID");
-        $uptadeair= $energyair -> execute(array(
-            "airKwh"=>$kwh,
-            "airMoney"=>$money,
-            "airID"=>3
+        $uptadeair = $energyair->execute(array(
+            "airKwh" => $kwh,
+            "airMoney" => $money,
+            "airID" => 3
         ));
     }
-    if($children["lightValue"]=="ON"){
-        $kwh=$children['lightKwh']+0.1;
-        $money=$kwh*0.2;
+    if ($children["lightValue"] == "ON") {
+        $kwh = $children['lightKwh'] + 0.1;
+        $money = $kwh * 0.2;
         $energylight = $db->prepare("UPDATE light SET lightKwh=:lightKwh,lightMoney=:lightMoney WHERE lightID=:lightID");
-        $uptadelight= $energylight -> execute(array(
-            "lightKwh"=>$kwh,
-            "lightMoney"=>$money,
-            "lightID"=>3
+        $uptadelight = $energylight->execute(array(
+            "lightKwh" => $kwh,
+            "lightMoney" => $money,
+            "lightID" => 3
         ));
     }
     ?>
@@ -61,45 +61,45 @@
         <div class="card-img-overlay">
             <h1 class="card-title bg-dark" style=" padding-top:30px; padding-bottom:30px; text-align: center;">Consumer Dashboard</h1>
             <h3 class="card-title bg-dark" style=" padding-top:30px; padding-bottom:30px; text-align: center; ">CHILDREN ROOM</h3>
-                <table class="table table-dark table-striped">
-                    <thead>
-                        <tr>
-                            <th scope="col">ID</th>
-                            <th scope="col">SENSOR</th>
-                            <th scope="col">VALUE</th>
-                            <th scope="col">LAST CHANGE TIME</th>
-                            <th scope="col">ENERGY SPEND</th>
-                            <th scope="col">MONEY</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <th scope="row"><?php echo $children['airID'] ?></th>
-                            <td>AIR CONDITION</td>
-                            <td><?php echo $children['airValue'] ?></td>
-                            <td><?php echo $children['airTime'] ?></td>
-                            <td><?php echo $children['airKwh']?> Kwh</td>
-                            <td><?php echo $children['airMoney']?> $</td>
-                        </tr>
-                        <tr>
-                            <th scope="row"><?php echo $children['heatID'] ?></th>
-                            <td>HEAT</td>
-                            <td><?php echo $children['heatValue'] ?></td>
-                            <td><?php echo $children['heatTime'] ?></td>
-                            <td>NO ENERGY FOR THAT</td>
-                            <td>NO MONEY FOR THAT</td>
+            <table class="table table-dark table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">SENSOR</th>
+                        <th scope="col">VALUE</th>
+                        <th scope="col">LAST CHANGE TIME</th>
+                        <th scope="col">ENERGY SPEND</th>
+                        <th scope="col">MONEY</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row"><?php echo $children['airID'] ?></th>
+                        <td>AIR CONDITION</td>
+                        <td><?php echo $children['airValue'] ?></td>
+                        <td><?php echo $children['airTime'] ?></td>
+                        <td><?php echo $children['airKwh'] ?> Kwh</td>
+                        <td><?php echo $children['airMoney'] ?> $</td>
+                    </tr>
+                    <tr>
+                        <th scope="row"><?php echo $children['heatID'] ?></th>
+                        <td>HEAT</td>
+                        <td><?php echo $children['heatValue'] ?></td>
+                        <td><?php echo $children['heatTime'] ?></td>
+                        <td>NO ENERGY FOR THAT</td>
+                        <td>NO MONEY FOR THAT</td>
 
-                        </tr>
-                        <tr>
-                            <th scope="row"><?php echo $children['lightID'] ?></th>
-                            <td>LIGHT</td>
-                            <td><?php echo $children['lightValue'] ?></td>
-                            <td><?php echo $children['lightTime'] ?></td>
-                            <td><?php echo $children['lightKwh']?> Kwh</td>
-                            <td><?php echo $children['lightMoney']?> $</td>
-                        </tr>
-                    </tbody>
-                </table>
+                    </tr>
+                    <tr>
+                        <th scope="row"><?php echo $children['lightID'] ?></th>
+                        <td>LIGHT</td>
+                        <td><?php echo $children['lightValue'] ?></td>
+                        <td><?php echo $children['lightTime'] ?></td>
+                        <td><?php echo $children['lightKwh'] ?> Kwh</td>
+                        <td><?php echo $children['lightMoney'] ?> $</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </div>
 
