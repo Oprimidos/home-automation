9817d7dafb31ffccc0a6b578c3aa081e0ac2dc60
Mustafa Esen <mesen1100@gmail.com>
2023-06-04 13:20:25 +0300

    Add connect to db kitchenP

--- kitchenP.php
+++ kitchenP.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Producer Dashboard</title>
     <link rel="stylesheet" href="assets/css/rooms.css">
     <header>
-  <?php include("navbarP.php"); ?>
-</header>
-<style>
+        <?php include("navbarP.php"); ?>
+    </header>
+    <style>
         body {
             background-image: url('./assets/images/kitchen.jpg');
         }
@@ -14,67 +15,88 @@
 </head>
 
 <body>
-  <div class="container">
+    <?php
+    $sqlkitchen = "SELECT * FROM viewconsumer WHERE roomID=2";
+    $querykitchen = $db->prepare($sqlkitchen);
+    $querykitchen->execute();
+    $kitchen = $querykitchen->fetch(PDO::FETCH_ASSOC); ?>
+    <div class="container">
         <br>
         <br>
         <br>
 
         <div class=" card text-bg-dark">
 
-        <div class="card-img-overlay">
-            <h1 class="card-title bg-dark" style=" padding-top:30px; padding-bottom:30px; text-align: center;">Producer Dashboard</h1>
-            <h3 class="card-title bg-dark" style=" padding-top:30px; padding-bottom:30px; text-align: center; ">KITCHEN</h3>
-    
-<table class="table table-dark table-striped">
-                <thead>
-                <tr>
-<th scope="col">ID</th>
-<th scope="col">SENSOR</th>
-<th scope="col">VALUE</th>
-<th scope="col">LAST CHANGE TIME</th>
-<th scope="col">ENERGY SPEND</th>
-<th scope="col">MONEY SPEND</th>
-<th></th>
+            <div class="card-img-overlay">
+                <h1 class="card-title bg-dark" style=" padding-top:30px; padding-bottom:30px; text-align: center;">Producer Dashboard</h1>
+                <h3 class="card-title bg-dark" style=" padding-top:30px; padding-bottom:30px; text-align: center; ">KITCHEN</h3>
 
-</tr>
-                </thead>
-                <tbody>
-                <tr>
-    <th scope="row"><?php echo $bedroom['airID'] ?></th>
-    <td>AIR CONDITION</td>
-    <td><?php echo $bedroom['airValue'] ?></td>
-    <td><?php echo $bedroom['airTime'] ?></td>
-    <td><?php echo $bedroom['airKwh'] ?> Kwh</td>
-    <td><?php echo $bedroom['airMoney']?> $</td>
-    <td>
-        <button type="button" class="btn btn-primary">Increase</button>
-        <button type="button" class="btn btn-danger">Decrease</button>
-    </td>
-</tr>
+                <table class="table table-dark table-striped">
+                    <thead>
+                        <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">SENSOR</th>
+                            <th scope="col">VALUE</th>
+                            <th scope="col">LAST CHANGE TIME</th>
+                            <th scope="col">ENERGY SPEND</th>
+                            <th scope="col">MONEY SPEND</th>
+                            <th></th>
 
-                    <tr>
-                        <th scope="row"><?php echo $bedroom['heatID'] ?></th>
-                        <td>HEAT</td>
-                        <td><?php echo $bedroom['heatValue'] ?></td>
-                        <td><?php echo $bedroom['heatTime'] ?></td>
-                        <td>NO ENEGRY FOR THAT</td>
-                        <td>NO MONEY FOR THAT</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <th scope="row"><?php echo $bedroom['lightID'] ?></th>
-                        <td>LIGHT</td>
-                        <td><?php echo $bedroom['lightValue'] ?></td>
-                        <td><?php echo $bedroom['lightTime'] ?></td>
-                        <td><?php echo $bedroom['lightKwh'] ?> Kwh</td>
-                        <td><?php echo $bedroom['lightMoney']?> $</td>
-                        <td scope="col"><button type="button" class="btn btn-primary">On</button> <button type="button" class="btn btn-danger">Off</button></td>
-                    </tr>
-                </tbody>
-            </table>
-    </div>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row"><?php echo $kitchen['airID'] ?></th>
+                            <td>AIR CONDITION</td>
+                            <td><?php echo $kitchen['airValue'] ?></td>
+                            <td><?php echo $kitchen['airTime'] ?></td>
+                            <td><?php echo $kitchen['airKwh'] ?> Kwh</td>
+                            <td><?php echo $kitchen['airMoney'] ?> $</td>
+                            <td>
+                                <button type="button" class="btn btn-primary">Increase</button>
+                                <button type="button" class="btn btn-danger">Decrease</button>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <th scope="row"><?php echo $kitchen['heatID'] ?></th>
+                            <td>HEAT</td>
+                            <td><?php echo $kitchen['heatValue'] ?></td>
+                            <td><?php echo $kitchen['heatTime'] ?></td>
+                            <td>NO ENEGRY FOR THAT</td>
+                            <td>NO MONEY FOR THAT</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <th scope="row"><?php echo $kitchen['lightID'] ?></th>
+                            <td>LIGHT</td>
+                            <td><?php echo $kitchen['lightValue'] ?></td>
+                            <td><?php echo $kitchen['lightTime'] ?></td>
+                            <td><?php echo $kitchen['lightKwh'] ?> Kwh</td>
+                            <td><?php echo $kitchen['lightMoney'] ?> $</td>
+                            <td scope="col">
+                                <button type="button" class="btn btn-primary">On</button>
+                                <button type="button" class="btn btn-danger">Off</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row"><?php echo $kitchen['humID'] ?></th>
+                            <td>HUMIDITY</td>
+                            <td><?php echo $kitchen['humValue'] ?></td>
+                            <td><?php echo $kitchen['humTime'] ?></td>
+                            <td><?php echo $kitchen['humKwh'] ?> Kwh</td>
+                            <td><?php echo $kitchen['humMoney'] ?> $</td>
+                            <td>
+                                <button type="button" class="btn btn-primary">Increase</button>
+                                <button type="button" class="btn btn-danger">Decrease</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
 
 
 
 </body>
+
 </html>
