e3ce6babc01f38e4435568c619498eb745f22fa2
Mustafa Esen <mesen1100@gmail.com>
2023-06-20 14:44:16 +0300

    Update Code login user for database updating

--- config/loginuser.php
+++ config/loginuser.php
@@ -1,53 +1,49 @@
-<?php 
+<?php
 include("connectdb.php");
 
-if(isset($_POST["loginP"])){
-    $userName = trim(htmlspecialchars($_POST["userName"]));
-$userPassword = trim(htmlspecialchars($_POST["userPassword"]));
-$userType = "Producer";
+if (isset($_POST["loginP"])) {
+    $userMail = trim(htmlspecialchars($_POST["userMail"]));
+    $userPassword = trim(htmlspecialchars($_POST["userPassword"]));
+    $userType = "Producer";
 
-$usersor = $db->prepare("SELECT * FROM users WHERE userName = :userName AND userPassword = :userPassword AND userType = :userType");
+    $usersor = $db->prepare("SELECT * FROM users WHERE userMail = :userMail AND userPassword = :userPassword AND userType = :userType");
 
-$usersor->bindParam(":userName", $userName);
-$usersor->bindParam(":userPassword", $userPassword);
-$usersor->bindParam(":userType", $userType);
+    $usersor->bindParam(":userMail", $userMail);
+    $usersor->bindParam(":userPassword", $userPassword);
+    $usersor->bindParam(":userType", $userType);
 
-$usergetir = $usersor->execute();
-$usergetir = $usersor->fetch(PDO::FETCH_ASSOC);
-$say = $usersor->rowCount();
+    $usergetir = $usersor->execute();
+    $usergetir = $usersor->fetch(PDO::FETCH_ASSOC);
+    $say = $usersor->rowCount();
     echo $say;
-    if($say==1){
-        $_SESSION["userName"]=$usergetir["userName"];
+    if ($say == 1) {
+        $_SESSION["userMail"] = $usergetir["userMail"];
+        $_SESSION["userHomeID"] = $usergetir["userHomeID"];
         header("Location:../homeP.php");
-    }
-    else{
+    } else {
         header("Location:../loginP.php?islem=no");
-
     }
-    
 }
-if(isset($_POST["login"])){
-    $userName = trim(htmlspecialchars($_POST["userName"]));
-$userPassword = trim(htmlspecialchars($_POST["userPassword"]));
-$userType = "Consumer";
+if (isset($_POST["login"])) {
+    $userMail = trim(htmlspecialchars($_POST["userMail"]));
+    $userPassword = trim(htmlspecialchars($_POST["userPassword"]));
+    $userType = "Consumer";
 
-$usersor = $db->prepare("SELECT * FROM users WHERE userName = :userName AND userPassword = :userPassword AND userType = :userType");
+    $usersor = $db->prepare("SELECT * FROM users WHERE userMail = :userMail AND userPassword = :userPassword AND userType = :userType");
 
-$usersor->bindParam(":userName", $userName);
-$usersor->bindParam(":userPassword", $userPassword);
-$usersor->bindParam(":userType", $userType);
+    $usersor->bindParam(":userMail", $userMail);
+    $usersor->bindParam(":userPassword", $userPassword);
+    $usersor->bindParam(":userType", $userType);
 
-$usergetir = $usersor->execute();
-$usergetir = $usersor->fetch(PDO::FETCH_ASSOC);
-$say = $usersor->rowCount();
+    $usergetir = $usersor->execute();
+    $usergetir = $usersor->fetch(PDO::FETCH_ASSOC);
+    $say = $usersor->rowCount();
     echo $say;
-    if($say==1){
-        $_SESSION["userName"]=$usergetir["userName"];
+    if ($say == 1) {
+        $_SESSION["userMail"] = $usergetir["userMail"];
+        $_SESSION["userHomeID"] = $usergetir["userHomeID"];
         header("Location:../home.php");
-    }
-    else{
+    } else {
         header("Location:../login.php?islem=no");
-
     }
-    
 }
--- login.php
+++ login.php
@@ -1,39 +1,47 @@
 <?php
-session_start();?>
+session_start(); ?>
 <!DOCTYPE html>
 <html>
+
 <head>
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Consumer Login</title>
     <link rel="stylesheet" href="assets/css/login.css">
     <style>
-      body {
-        margin: 20px;
-      }
+        body {
+            margin: 20px;
+        }
     </style>
-   
+
 </head>
+
 <body>
     <div class="container">
-        
-        <?php if(isset($error)) { ?>
+
+        <?php if (isset($error)) { ?>
             <p class="error"><?php echo $error; ?></p>
         <?php } ?>
         <div class="login-form">
             <form class="login" method="POST" action="config/loginuser.php">
                 <div class="form-group">
-                <h1>Consumer Login</h1>
-                    <label for="username">Username:</label>
-                    <input type="text" id="username" name="userName" value="admin" required>
+                    <h1>Consumer Login</h1>
+                    <label for="userMail">Email Address:</label>
+                    <input type="text" id="userMail" name="userMail" value="admin" required>
                 </div>
-<br>
+                <br>
                 <div class="form-group">
                     <label for="password">Password:&nbsp;</label>
-                    <input type="password" id="password" name="userPassword" value="12345"required>
+                    <input type="password" id="password" name="userPassword" value="12345" required>
                 </div>
-                <button type="submit" name="login" value="Login">Login</button>
-            </form>
+                <br>
+                
+                <button type="submit" name="login" value="login">Login</button>
+                <button type="submit" name="register" value="register">Register</button>
         </div>
+        </form>
+    </div>
     </div>
 </body>
+
 </html>
