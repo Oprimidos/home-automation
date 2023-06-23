9353ae4a3d2455206e3202dcb1278dfbe339b745
Mustafa Esen <mesen1100@gmail.com>
2023-06-23 15:46:19 +0300

    Edit login for Producer

--- loginP.php
+++ loginP.php
@@ -3,6 +3,7 @@ session_start();?>
 <!DOCTYPE html>
 <html>
 <head>
+    <link rel="shortcut icon" href="assets/images/favicon-home.png" type="image/x-icon">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Producer Login</title>
     <link rel="stylesheet" href="assets/css/login.css">
@@ -24,15 +25,17 @@ session_start();?>
             <form class="login" method="POST" action="config/loginuser.php">
                 <div class="form-group">
                 <h1>Producer Login</h1>
-                    <label for="username">Username:</label>
-                    <input type="text" id="username" name="userName" value="admin" required>
+                    <label for="userMail">Email Address:</label>
+                    <input type="text" id="userMail" name="userMail" value="admin" required>
                 </div>
 <br>
                 <div class="form-group">
                     <label for="password">Password:&nbsp;</label>
                     <input type="password" id="password" name="userPassword" value="12345" required>
                 </div>
+                <br>
                 <button type="submit" name="loginP" value="Login">Login</button>
+                <button type="submit" name="registerP" value="Register">Register</button>
             </form>
         </div>
     </div>
