cb29db251caf7feb12bdd15e8a1c4c587734a5f8
Mustafa Esen <mesen1100@gmail.com>
2023-06-23 18:26:32 +0300

    Add a button for sensor change

--- roomP.php
+++ roomP.php
@@ -4,6 +4,8 @@
 <head>
     <link rel="stylesheet" href="assets/css/rooms.css">
     <link rel="shortcut icon" href="assets/images/favicon-home.png" type="image/x-icon">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <header>
         <?php include("navbarP.php"); ?>
     </header>
@@ -13,18 +15,18 @@
     $sqlroom->execute();
     $room = $sqlroom->fetch(PDO::FETCH_ASSOC);
     ?>
-    <title><?php echo$room["roomName"]?> Dashboard</title>
+    <title><?php echo $room["roomName"] ?> Dashboard</title>
     <style>
         body {
             background-image: url('<?php echo $room["roomPhoto"] ?>');
         }
     </style>
-    <?php 
-    if(!($_SESSION["userHomeID"]==$room["homeID"])){
+    <?php
+    if (!($_SESSION["userHomeID"] == $room["homeID"])) {
         session_destroy();
         header("Location:loginP.php?islem=no");
     }
-    
+
     ?>
     <?php
     /*$dataPoints = array();
@@ -79,40 +81,64 @@
             <h1 class="card-title bg-dark" style=" padding-top:30px; padding-bottom:30px; text-align: center;">Producer Dashboard</h1>
             <h3 class="card-title bg-dark" style=" padding-top:30px; padding-bottom:30px; text-align: center; "><?php echo $room["roomName"] ?></h3>
             <div class="row col-12">
-                
-                    <table class="table table-dark table-striped" style="height: 250px;">
-                        <thead>
+
+                <table class="table table-dark table-striped" style="height: 250px;">
+                    <thead>
+                        <tr>
+                            <th scope="col">SENSOR</th>
+                            <th scope="col">VALUE</th>
+                            <th scope="col">LAST CHANGE TIME</th>
+                            <th scope="col">ENERGY SPEND</th>
+                            <th scope="col">MONEY</th>
+                            <th scope="col">BUTTONS</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+
+                        $sqlsensor = $db->prepare('SELECT * from sensor WHERE sensorRoomID=:sensorRoomID');
+                        $sqlsensor->bindParam(':sensorRoomID', $_GET["roomID"]);
+                        $sqlsensor->execute();
+                        $sensors = $sqlsensor->fetchAll(PDO::FETCH_ASSOC);
+                        foreach ($sensors as $sensor) {
+                        ?>
                             <tr>
-                                <th scope="col">SENSOR</th>
-                                <th scope="col">VALUE</th>
-                                <th scope="col">LAST CHANGE TIME</th>
-                                <th scope="col">ENERGY SPEND</th>
-                                <th scope="col">MONEY</th>
+                                <td><?php echo $sensor['sensorType'] ?></td>
+                                <td><?php echo $sensor['sensorValue'] ?></td>
+                                <td><?php echo $sensor['sensorTime'] ?></td>
+                                <td><?php echo $sensor['sensorKwh'] ?> Kwh</td>
+                                <td><?php echo $sensor['sensorMoney'] ?> $</td>
+                                <td>
+                                    <form action="config/deneme.php" method="post">
+                                    <div class="btn-group" role="group" aria-label="Basic example">
+                                        <?php
+                                        if ($sensor["sensorType"] == "Light") {
+                                            echo '<button type="submit" class="btn btn-outline-light" name="lighton">ON</button>
+                                        <button type="submit" class="btn btn-outline-light" name="lightoff">OFF</button>';
+                                        }
+                                        elseif ($sensor["sensorType"]=="Air Condition") {
+                                            echo '<button type="submit" class="btn btn-outline-light" name="airon">ON</button>
+                                                <button type="submit" class="btn btn-outline-light" name="airup">INCREASE</button>
+                                                <button type="submit" class="btn btn-outline-light" name="airdown">DECREASE</button>
+                                                <button type="submit" class="btn btn-outline-light" name="airof">OFF</button>';
+                                        }
+                                        elseif($sensor["sensorType"]=="Humidity"){
+                                            echo '<button type="submit" class="btn btn-outline-light" name="humup">INCREASE</button>
+                                        <button type="submit" class="btn btn-outline-light" name="humdown">DECREASE</button>';
+                                        }
+                                        ?>
+                                        </div>
+                                    </form>
+
+                                </td>
+
                             </tr>
-                        </thead>
-                        <tbody>
-                            <?php
-                            
-                            $sqlsensor = $db->prepare('SELECT * from sensor WHERE sensorRoomID=:sensorRoomID');
-                            $sqlsensor->bindParam(':sensorRoomID', $_GET["roomID"]);
-                            $sqlsensor->execute();
-                            $sensors = $sqlsensor->fetchAll(PDO::FETCH_ASSOC);
-                            foreach ($sensors as $sensor) { 
-                            ?>
-                                <tr>
-                                    <td><?php echo $sensor['sensorType'] ?></td>
-                                    <td><?php echo $sensor['sensorValue'] ?></td>
-                                    <td><?php echo $sensor['sensorTime'] ?></td>
-                                    <td><?php echo $sensor['sensorKwh'] ?> Kwh</td>
-                                    <td><?php echo $sensor['sensorMoney'] ?> $</td>
+                        <?php } ?>
 
-                                </tr>
-                            <?php } ?>
+                    </tbody>
+                </table>
 
-                        </tbody>
-                    </table>
-                
-                
+
             </div>
         </div>
     </div>
