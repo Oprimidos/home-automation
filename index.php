84e1516d1052d28c127cceb104b081721f67fd54
Mazlum Arcanlı <83984387+Oprimidos@users.noreply.github.com>
2023-06-24 16:42:42 +0300

    index yeni dizayn

--- index.php
+++ index.php
@@ -1,37 +1,73 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
     <title>Home Automation Index</title>
     <link rel="shortcut icon" href="assets/images/favicon-home.png" type="image/x-icon">
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/css/index.css">
-
-</head>
-
-<body class="main-layout">
-    <section class="banner_main">
-        <div class="container-fluid">
-            <div class="row d_flex">
-                <div class="col-md-7">
-                    <div class="text-bg">
-                        <div class="padding_lert">
-                            <h1>Home Automation <br>Landing Page</h1>
-                            <a href="login.php">Consumer</a>
-                            <a href="loginP.php">Producer</a>
-                        </div>
-                    </div>
+        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script> 
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
+        <link href="assets/css/index.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        
+        <!-- Masthead-->
+        <header class="masthead">
+            <div class="container">
+                <div class="masthead-subheading">Welcome To M^2 HOME AUTOMATION SYSTEMS</div>
+                <div class="masthead-heading text-uppercase">We are improving your house</div>
+                <a class="btn btn-primary btn-xl text-uppercase" href="login.php">CONSUMER</a>
+                <a class="btn btn-primary btn-xl text-uppercase" href="loginP.php">PRODUCER</a>
+            </div>
+        </header>
+        <!-- Services-->
+        <section class="page-section" id="services">
+            <div class="container">
+                <div class="text-center">
+                    <h2 class="section-heading text-uppercase">Benefits</h2>
+                    <h3 class="section-subheading text-muted">What will you gain after start the use M^2</h3>
                 </div>
-                <div class="col-md-5 bah">
-                    <div class="bann_img">
-                        <figure><img src="assets/images/bann_img.png" alt="#" /></figure>
+                <div class="row text-center">
+                    <div class="col-md-4">
+                        <span class="fa-stack fa-4x">
+                            <i class="fas fa-circle fa-stack-2x text-primary"></i>
+                            <i class="fas fa-house fa-stack-1x fa-inverse"></i>
+                        </span>
+                        <h4 class="my-3">Home</h4>
+                        <p class="text-muted">Make your home smart</p>
+                    </div>
+                    <div class="col-md-4">
+                        <span class="fa-stack fa-4x">
+                            <i class="fas fa-circle fa-stack-2x text-primary"></i>
+                            <i class="fas fa-laptop fa-stack-1x fa-inverse"></i>
+                        </span>
+                        <h4 class="my-3">Manage</h4>
+                        <p class="text-muted">Control your home easily</p>
+                    </div>
+                    <div class="col-md-4">
+                        <span class="fa-stack fa-4x">
+                            <i class="fas fa-circle fa-stack-2x text-primary"></i>
+                            <i class="fas fa-dollar fa-stack-1x fa-inverse"></i>
+                        </span>
+                        <h4 class="my-3">Cheap</h4>
+                        <p class="text-muted">Save lots of money</p>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
-
-
-</body>
-
+        </section>
+       
+      
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="assets/js/scripts.js"></script>
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <!-- * *                               SB Forms JS                               * *-->
+        <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+    </body>
 </html>
