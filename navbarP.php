47371df2558e29600ee58a4b8534bd7746ea6b22
Mustafa Esen <Mesen1100@gmail.com>
2023-07-06 23:22:58 +0300

    Update Navbar

--- navbarP.php
+++ navbarP.php
@@ -30,7 +30,7 @@
     <nav class="navbar navbar-expand-lg bg-primary">
         <div class="container-fluid">
             <img src="assets/images/M^2-1.png" alt="Logo" width="30" height="24">
-            <a class="navbar-brand" href="#">Navbar</a>
+            <a class="navbar-brand" href="homeP.php">Navbar</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
